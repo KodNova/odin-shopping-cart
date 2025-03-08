693c6f4ef30e6370846125eac1ff77748969abcf
Source-Kod <Source-Kod@protonmail.com>
2025-03-07 19:58:30 -0600

    add: basic Homepage

--- src/componets/Header.tsx
+++ src/componets/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="flex justify-between border-1 border-black p-2">
-      <h1>Logo</h1>
+      <h1 className="text-2xl">Minimalist Menswear</h1>
       <div className="flex gap-4">
         <p>Store</p>
         <p>Cart</p>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,12 @@ export default function Homepage() {
   return (
     <>
       <Header />
-      <h1>Welcome</h1>
+      <div className="flex justify-center">
+        <div className="flex flex-col items-center">
+          <h1 className="text-2xl">Welcome to Minimalist Menswear</h1>
+          <p>Curated collection of timeless essentials for the modern man.</p>
+        </div>
+      </div>
     </>
   );
 }
