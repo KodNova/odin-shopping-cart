f53ae822078cae1c522eec9588e2fe0385db17f2
Source-Kod <Source-Kod@protonmail.com>
2025-03-16 01:10:47 -0500

    add: basic container for shop items

--- src/componets/Header.tsx
+++ src/componets/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     <div className="flex justify-between border-1 border-black p-2">
       <h1 className="text-2xl">Minimalist Menswear</h1>
       <div className="flex gap-4">
-        <Link to="store">Store</Link>
+        <Link to="shop">Shop</Link>
         <Link to="cart">Cart</Link>
       </div>
     </div>
--- src/componets/ShopItemContainer.tsx
+++ src/componets/ShopItemContainer.tsx
@@ -1,7 +1,24 @@
 import useFetchItems from "../hooks/usefetchItems";
+import { ShopItem } from "../types";
 
 export default function ShopItemContainer() {
-  const ShopItemData = useFetchItems();
-  console.log(ShopItemData);
-  return <p>Shop Items</p>;
+  const items = useFetchItems();
+
+  if (!items) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <div className="flex">
+        {items.map((item: ShopItem) => (
+          <div className="w-1/5" key={item.id}>
+            <h3>{item.title}</h3>
+            <p>${item.price}</p>
+            <img className="w-30" src={item.image} alt={item.title} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
