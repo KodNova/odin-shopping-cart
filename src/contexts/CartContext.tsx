8850b78d2e0a6602eebf35fd3d4f3d6c7c4cce25
Source-Kod <Source-Kod@protonmail.com>
2025-04-11 15:21:02 -0500

    add: addToCart function + three helper functions

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -20,12 +20,40 @@ export default function CartContextProvider({
 }: CartContextProviderProps) {
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
+  // addToCart helper 1
+  const checkIfItemExist = (newItem: ShopItem): boolean =>
+    cartItems.some((item) => newItem.id === item.id);
+
+  // addToCart helper 2
+  const updateExistingItemQuanity = (
+    currentCart: CartItem[],
+    itemToUpdate: ShopItem,
+    quantityToAdd: number,
+  ): CartItem[] => {
+    return currentCart.map((cartItem: CartItem) =>
+      cartItem.id === itemToUpdate.id
+        ? { ...cartItem, quantity: cartItem.quantity + quantityToAdd }
+        : cartItem,
+    );
+  };
+
+  // addToCart helper 3
+  const addNewItemToCart = (
+    currentCart: CartItem[],
+    itemToAdd: ShopItem,
+    quantity: number,
+  ): CartItem[] => {
+    const newItem: CartItem = { ...itemToAdd, quantity };
+    return [...currentCart, newItem];
+  };
+
+  // add to cart function
   const addToCart = (item: ShopItem, quantity: number) => {
-    const newItem: CartItem = { ...item, quantity };
-    setCartItems((prevCartItems) => {
-      const newCartItems = [...prevCartItems, newItem];
-      return newCartItems;
-    });
+    checkIfItemExist(item)
+      ? setCartItems(updateExistingItemQuanity(cartItems, item, quantity))
+      : setCartItems(addNewItemToCart(cartItems, item, quantity));
+    console.log("addToCart ran");
+    console.log(cartItems);
   };
 
   return (
