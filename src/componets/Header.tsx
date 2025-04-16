4b09a6a9514630af1814292edf78eb884fb6deeb
Source-Kod <Source-Kod@protonmail.com>
2025-04-16 18:29:44 -0500

    fix: header logo links to homepage

--- src/componets/Header.tsx
+++ src/componets/Header.tsx
@@ -2,7 +2,9 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div className="flex justify-between border-1 border-black p-2">
-      <h1 className="text-2xl">Minimalist Menswear</h1>
+      <Link to="/" className="text-2xl">
+        Minimalist Menswear
+      </Link>
       <div className="flex gap-4">
         <Link to="/shop">Shop</Link>
         <Link to="/cart">Cart</Link>
