242e43c8d9371f450c35ef50f9dcb3dc03210ad9
Source-Kod <Source-Kod@protonmail.com>
2025-04-16 19:01:42 -0500

    add: pattern background

--- src/componets/Header.tsx
+++ src/componets/Header.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 export default function Header() {
   return (
-    <div className="flex justify-between border-1 border-black p-2">
+    <div className="flex justify-between border-1 border-black bg-slate-400 p-2">
       <Link to="/" className="text-2xl">
         Minimalist Menswear
       </Link>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,11 @@ export default function Homepage() {
     <>
       <Header />
       <div className="flex justify-center">
-        <div className="flex flex-col items-center">
-          <h1 className="text-2xl">Welcome to Minimalist Menswear</h1>
-          <p>Curated collection of timeless essentials for the modern man.</p>
+        <div className="w-30% mt-6 flex justify-center rounded-2xl bg-slate-400 p-3">
+          <div className="flex flex-col items-center">
+            <h1 className="text-2xl">Welcome to Minimalist Menswear</h1>
+            <p>Curated collection of timeless essentials for the modern man.</p>
+          </div>
         </div>
       </div>
     </>
