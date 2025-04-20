58e10bd67e035622fb0855ae8ef5752389d78802
Source-Kod <Source-Kod@protonmail.com>
2025-04-19 20:02:19 -0500

    add: cart item counter on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { buttonVariants } from "@/components/ui/button";
+import useCartContext from "@/hooks/useCartContext";
 import { Link } from "react-router-dom";
 export default function Header() {
+  const { cartItems } = useCartContext();
   return (
     <div className="mb-6 flex items-center justify-between border-1 border-black bg-slate-400 p-2 shadow-lg shadow-black">
       <Link to="/">
@@ -12,9 +14,14 @@ export default function Header() {
         <Link to="/shop" className={buttonVariants({ variant: "link" })}>
           <p className="text-2xl">Shop</p>
         </Link>
-        <Link to="/cart" className={buttonVariants({ variant: "link" })}>
-          <p className="text-2xl">Cart</p>
-        </Link>
+        <div className="flex items-center gap-1">
+          <Link to="/cart" className={buttonVariants({ variant: "link" })}>
+            <p className="text-2xl">Cart</p>
+          </Link>
+          <p className="h-10 w-10 rounded-full border-1 border-black p-1 text-center text-2xl">
+            {cartItems.length}
+          </p>
+        </div>
       </div>
     </div>
   );
